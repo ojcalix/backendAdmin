3378bff55ad37542e45889442e1bbd0fe1b7f090
ojcalix <ojcalix@outlook.es>
2026-08-11 16:57:46 -0600

    Cambios agregados despues de contabilidad

--- config/db.js
+++ config/db.js
@@ -1,4 +1,10 @@
 const mysql = require('mysql2');
+
+console.log('MYSQLHOST:', process.env.MYSQLHOST);
+console.log('MYSQLPORT:', process.env.MYSQLPORT);
+console.log('MYSQLUSER:', process.env.MYSQLUSER);
+console.log('MYSQLDATABASE:', process.env.MYSQLDATABASE);
+
 // Configuración de la conexión a la base de datos MySQL
 const db = mysql.createConnection({
     host: process.env.MYSQLHOST,
